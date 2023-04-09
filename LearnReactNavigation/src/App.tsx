04f1321551e2c3b379381c55665685e7cac848ed
mywhzy <cmj2393a@naver.com>
2023-04-09 21:09:01 +0900

    init: navigation 라이브러리 설치 및 초기 세팅

---
+++ LearnReactNavigation/src/App.tsx
@@ -0,0 +1,30 @@
+import { registerRootComponent } from 'expo';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+registerRootComponent(App);
---
+++ LearnReactNavigation/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Button title="Detail 열기" />
+    </View>
+  );
+};
+
+export default HomeScreen;
